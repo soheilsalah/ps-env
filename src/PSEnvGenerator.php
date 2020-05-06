50cf10589729429a9943bda67a204e31a9935672
soheilsalah <soheilsalah2@gmail.com>
2020-05-06 05:31:45 +0200

    PSEnv - First Commit

---
+++ src/PSEnvGenerator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PandaStudios\PSEnv;
+
+class PSEnvGenerator
+{
+    public function create()
+    {
+        echo 'creating';
+    }
+}
---
+++ src/Providers/PSEnvServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PandaStudios\PSEnv\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PSEnvServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+        # code...
+    }
+}
